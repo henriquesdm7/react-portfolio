8ccb62af324dcb009bb119c64c6214a788fe4c00
Henrique Mattos <49873860+henriquesdm7@users.noreply.github.com>
2022-10-24 10:51:38 -0300

    feat: cursor pointer

--- client/src/components/buttons/DarkModeButton.jsx
+++ client/src/components/buttons/DarkModeButton.jsx
@@ -5,7 +5,7 @@ const DarkModeButton = ({ darkMode, toggleDarkMode }) => {
     return (
         <button
             onClick={toggleDarkMode}
-            className="fixed top-3 right-0 p-3 bg-slate-700 rounded-tl-full rounded-bl-full"
+            className="fixed top-3 right-0 p-3 bg-slate-700 rounded-tl-full rounded-bl-full cursor-pointer"
             // className="fixed top-[50%] left-[50%] bg-slate-700 rounded-full"
         >
             <AnimatedDarkModeIcon isDarkMode={darkMode} />
--- client/src/components/icons/AnimatedDarkModeIcon.jsx
+++ client/src/components/icons/AnimatedDarkModeIcon.jsx
@@ -37,7 +37,7 @@ const AnimatedDarkModeIcon = ({ isDarkMode }) => {
             strokeLinecap="round"
             strokeLinejoin="round"
             style={{
-                cursor: "default",
+                cursor: "pointer",
                 ...svgContainerProps
             }}
         >
